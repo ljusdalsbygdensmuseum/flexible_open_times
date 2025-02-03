710b89535518dddc1f6b2c894e21f8be3e147bd6
Ina Jobbet <ina.eklund@ljm.se>
2025-02-03 13:09:16 +0100

    date range will now show all selected dates

--- src/components/foh-date-range.tsx
+++ src/components/foh-date-range.tsx
@@ -1,52 +1,77 @@
-import { useState } from "react"
-import { DatePicker, PanelBody } from "@wordpress/components"
-import { DatesRange } from "./foh-metabox-temporary-types"
+import { useState } from 'react'
+import { DatePicker, PanelBody } from '@wordpress/components'
+import { DatesRange } from './foh-metabox-temporary-types'
+import { DatePickerEvent } from '@wordpress/components/build-types/date-time/types'
 
 interface Props {
-    dates: DatesRange
-    input: HTMLInputElement
+	dates: DatesRange
+	input: HTMLInputElement
 }
-export function FOHDateRange({dates, input}: Props){
-    const [dateRange, setDateRange] = useState(dates)
+export function FOHDateRange({ dates, input }: Props) {
+	const getAllDates = (range: DatesRange) => {
+		let allDatesArray: DatePickerEvent[] = []
+		let loopDate = new Date(range.start.date)
 
-    const onDateClick = (newDate: Date)=>{
-        setDateRange((oldDates) => {
-            let changeDates = oldDates
-            if(new Date(newDate).toDateString() == new Date(oldDates.start.date).toDateString() || new Date(newDate).toDateString() == new Date(oldDates.end.date).toDateString()){
-                return oldDates
-            }
-            if (new Date(newDate) < new Date(oldDates.start.date)) {
-                changeDates.start.date = new Date(newDate)
-            }
-            if (new Date(newDate) > new Date(oldDates.end.date)) {
-                changeDates.end.date = new Date(newDate)
-            }
-            if (new Date(newDate) > new Date(oldDates.start.date) && new Date(newDate) < new Date(oldDates.end.date)) {
-                const endRange = new Date(oldDates.end.date).getTime() - new Date(newDate).getTime()
-                const startRange = new Date(newDate).getTime() - new Date(oldDates.start.date).getTime()
+		while (loopDate <= new Date(range.end.date)) {
+			allDatesArray.push({ date: new Date(loopDate) })
+			loopDate = new Date(loopDate.setDate(loopDate.getDate() + 1))
+		}
+		return allDatesArray
+	}
 
-                if (startRange >= endRange) {
-                    changeDates.end.date = new Date(newDate)
-                }else if (startRange < endRange) {
-                    changeDates.start.date = new Date(newDate)
-                }
-            }
-            
-            return changeDates
-        })
+	const [dateRange, setDateRange] = useState(dates)
+	const [allDates, setAllDates] = useState(getAllDates(dateRange))
 
-        input.value = JSON.stringify(dateRange)
-    }
-    return(
-        <>
-        <PanelBody>
-            <DatePicker
-            startOfWeek={1}
-            currentDate={null}
-            events={[dateRange.start, dateRange.end]}
-            onChange={(newDate) => onDateClick(new Date(newDate))}
-            />
-        </PanelBody>
-        </>
-    )
+	const onDateClick = (newDate: Date) => {
+		setDateRange((oldDates) => {
+			let changeDates = oldDates
+			if (
+				new Date(newDate).toDateString() ==
+					new Date(oldDates.start.date).toDateString() ||
+				new Date(newDate).toDateString() ==
+					new Date(oldDates.end.date).toDateString()
+			) {
+				return oldDates
+			}
+			if (new Date(newDate) < new Date(oldDates.start.date)) {
+				changeDates.start.date = new Date(newDate)
+			}
+			if (new Date(newDate) > new Date(oldDates.end.date)) {
+				changeDates.end.date = new Date(newDate)
+			}
+			if (
+				new Date(newDate) > new Date(oldDates.start.date) &&
+				new Date(newDate) < new Date(oldDates.end.date)
+			) {
+				const endRange =
+					new Date(oldDates.end.date).getTime() - new Date(newDate).getTime()
+				const startRange =
+					new Date(newDate).getTime() - new Date(oldDates.start.date).getTime()
+
+				if (startRange >= endRange) {
+					changeDates.end.date = new Date(newDate)
+				} else if (startRange < endRange) {
+					changeDates.start.date = new Date(newDate)
+				}
+			}
+			setAllDates(() => {
+				return getAllDates(changeDates)
+			})
+			return changeDates
+		})
+
+		input.value = JSON.stringify(dateRange)
+	}
+	return (
+		<>
+			<PanelBody>
+				<DatePicker
+					startOfWeek={1}
+					currentDate={null}
+					events={allDates}
+					onChange={(newDate) => onDateClick(new Date(newDate))}
+				/>
+			</PanelBody>
+		</>
+	)
 }
