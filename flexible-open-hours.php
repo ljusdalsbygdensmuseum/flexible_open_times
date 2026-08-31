34eb3b12a45bdbabf0f780b13b555dcb4e9ff167
Ina Eklund <theartflower@gmail.com>
2026-08-31 10:44:36 +0200

    forgot the domain

--- flexible-open-hours.php
+++ flexible-open-hours.php
@@ -66,7 +66,7 @@ class FlexibleOpenHours
 ?>
         <div class="wrap">
             <h1><?php _e('Normal Open Hours', 'foh-domain'); ?></h1>
-            <p class="description"><?php _e('fill in your open hours. If all are left empty it will say it is closed') ?></p>
+            <p class="description"><?php _e('fill in your open hours. If all are left empty it will say it is closed', 'foh-domain') ?></p>
             <form action="options.php" method="POST">
                 <?php
                 settings_errors();
