092a9eb5e3fca84bf4d3240d6d01e63870b67e0c
Ina Eklund <theartflower@gmail.com>
2026-08-31 10:50:03 +0200

    added titles and descriptions

--- build/metabox_temporary.asset.php
+++ build/metabox_temporary.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '095bd652037946437d01');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'db697bad3f3f8e764934');
